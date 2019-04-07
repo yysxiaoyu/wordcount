f1fcd7fe77737d55ffad444467be82740b0a4a21
yysxiaoyu <yysxiaoyu@hotmail.com>
2019-04-07 14:54:51 +0800

    添加了about页面

--- wordcount/function.py
+++ wordcount/function.py
@@ -20,3 +20,6 @@ def count(request):
     return render(request, 'count.html',
                   {'count':total_count,'text':user_text,
                    'wordict':word_dict,'sorted':sorted_dict})
+
+def about(request):
+    return  render(request, 'about.html')
